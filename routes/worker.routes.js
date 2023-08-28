c47a5e2520b0f2caab0d01181ce9077e227178c2
shizoevich <yelyzaveta.pysarenko@nure.ua>
2023-08-28 22:39:13 +0300

    test

--- routes/worker.routes.js
+++ routes/worker.routes.js
@@ -5,7 +5,9 @@ const workerController = require('../controller/worker.controller');
 
 router.post('/worker',passport.authenticate('jwt', { session: false }), workerController.createWorker);
 router.get('/worker/:id',passport.authenticate('jwt', { session: false }), workerController.getWorker);
-router.get('/workers',passport.authenticate('jwt', { session: false }), workerController.getWorkers);
+router.get('/workers', workerController.getWorkers);
+
+//router.get('/workers',passport.authenticate('jwt', { session: false }), workerController.getWorkers);
 router.put('/worker/:id',passport.authenticate('jwt', { session: false }), workerController.updateWorker);
 router.delete('/worker/:id',passport.authenticate('jwt', { session: false }), workerController.deleteWorker);
 
