02f997987b1d04534413909ddcb487c7fbd1b595
shizoevich <yelyzaveta.pysarenko@nure.ua>
2023-08-29 06:58:52 +0300

    commit message1

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const app = express()
 app.use(express.json())
 require('./config/passport')(passport);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
