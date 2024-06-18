a6a5bbfd2c16ae77d1e77ed3c4b428a80706bf27
shizoevich <94996568+shizoevich@users.noreply.github.com>
2024-06-18 11:21:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const express = require('express')
+const express = require('express');
 const passport = require('passport');
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 const workerRouter = require('./routes/worker.routes');
 const ownerRouter = require('./routes/owner.routes');
 const iotRouter = require('./routes/iot.routes');
-const workerdataRouter = require('./routes/workerdata.routes')
+const workerdataRouter = require('./routes/workerdata.routes');
 const sensordataRouter = require('./routes/sensordata.routes');
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 require('./config/passport')(passport);
-
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: '*' }));
 app.use(passport.initialize());
-app.use('/api', sensordataRouter)
-app.use('/api', ownerRouter)
-app.use('/api', iotRouter)
-app.use('/api', workerRouter)
-app.use('/api', workerdataRouter)
+app.use('/api', sensordataRouter);
+app.use('/api', ownerRouter);
+app.use('/api', iotRouter);
+app.use('/api', workerRouter);
+app.use('/api', workerdataRouter);
+
 app.listen(PORT, () => {
-    console.log(`kwak ${PORT}`)
-  })
+  console.log(`Server is running on port ${PORT}`);
+});
