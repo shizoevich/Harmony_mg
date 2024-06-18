9984d50c04d27e320046d9af8956ca245eb1a69d
shizoevich <yelyzaveta.pysarenko@nure.ua>
2024-06-18 17:04:44 +0300

    Resolved merge conflicts

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'postgres',
-    password: '1234',
+    password: '12345678',
     host: 'localhost',
     port: 5432,
     database: 'psychoemotional'
